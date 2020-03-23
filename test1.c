4b1bcecc0f5c1cddd4d77b03d2d3cafbee2432ec
Guodong Xu <guodong.xu@linaro.com>
2020-03-23 03:47:32 +0000

    Add routines to test Arm SVE intrinsics
    Signed-off-by: Guodong Xu <guodong.xu@linaro.com>

--- test1.c
+++ test1.c
@@ -1,10 +1,13 @@
 #ifdef __ARM_FEATURE_SVE
 #include <arm_sve.h>
 #else
-#error "No Arm SVE support"
+#error "No Arm SVE intrinsics support in compiler"
 #endif /* __ARM_FEATURE_SVE */
 
 #include <stdio.h>
+
+#define __START_TRACE() {asm volatile (".inst 0x2520e020");}
+#define __STOP_TRACE() {asm volatile (".inst 0x2520e040");}
 
 void testSVE()
 {
@@ -15,8 +18,35 @@ void testSVE()
 	return;
 }
 
+void example_sum_squares( int N, int * sum)
+{
+	svbool_t pred_N;
+	svint32_t svN_tmp;
+	svbool_t p_all = svptrue_b32();
+	svint32_t acc = svdup_s32(0);
+
+	if (N > 0) {
+		svN_tmp = svindex_s32(N, -1);
+		pred_N = svcmpgt( p_all, svN_tmp, 0);
+
+		do {
+			acc = svmla_m( pred_N, acc, svN_tmp, svN_tmp);
+			svN_tmp = svsub_x( p_all, svN_tmp, svcntw());
+			pred_N = svcmpgt( p_all, svN_tmp, 0);
+		} while (svptest_first( p_all, pred_N));
+	}
+	*sum = (int) svaddv( p_all, acc);
+}
+
 void main()
 {
+	int sum;
+
 	testSVE();
 	printf("hello world \n");
+	printf("vector length (bytes): %d\n", svcntb());
+	__START_TRACE();
+	example_sum_squares(1000, &sum);
+	__STOP_TRACE();
+	printf("sum %d\n", sum);
 }
